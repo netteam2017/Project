8cf2073debcdd80ca5074f209b5e175d1b349b3c
Kate Sterpu <sterekaterina@inbox.ru>
2017-11-17 03:33:57 +0400

    метод добавления не до конца сделан, для поддерева не работает

---
+++ Desktop/Tree-master/src/test/java/testNode.java
@@ -0,0 +1,6 @@
+/**
+ * Created by user on 17.11.2017.
+ */
+public class testNode {
+
+}
---
+++ Desktop/test/src/main/java/Test.java
@@ -0,0 +1,7 @@
+/**
+ * Created by user on 15.11.2017.
+ */
+import org.junit.test;
+public class Test {
+
+}
